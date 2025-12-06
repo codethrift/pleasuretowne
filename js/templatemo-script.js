2d37928867337e2c12117e6c0b82219fc417634e
codethrift <codethrift@users.noreply.github.com>
2025-12-06 14:11:14 -0500

    Removed and updated photos and script.

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -5,28 +5,14 @@ const initBg = (autoplay = true) => {
         '03.jpg',
         '04.jpg',
         '05.jpg',
-        '06.jpg',
         '07.jpg',
         '08.jpg',
-        '09.jpg',
         '10.jpg',
-        '11.jpg',
-        '12.jpg',
         '13.jpg',
-        '14.jpg',
-        '15.jpg',
-        '16.jpg',
         '17.jpg',
-        '18.jpg',
-        '19.jpg',
-        '20.jpg',
         '21.jpg',
-        '22.jpg',
-        '23.jpg',
         '24.jpg',
-        '25.jpg',
-        '40performance.jpg',
-        '01-backlight.jpg'
+        '40performance.jpg'
     ];
     const bgImgs = bgImgsNames.map(img => "img/" + img);
 
